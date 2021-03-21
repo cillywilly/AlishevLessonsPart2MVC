446b48afc85c0018c50fd6533a6c45bb95706750
AIllarionov <mokolot@mail.ru>
2021-03-21 22:05:11 +0300

    MVC lesson 7

--- src/main/java/ru/illarionov/springcourse/config/MySpringMVCDispatcherServletInitializer.java
+++ src/main/java/ru/illarionov/springcourse/config/MySpringMVCDispatcherServletInitializer.java
@@ -1,12 +1,16 @@
 package ru.illarionov.springcourse.config;
 
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 public class MySpringMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+        return null;
     }
 
     @Override
@@ -17,5 +21,16 @@ public class MySpringMVCDispatcherServletInitializer extends AbstractAnnotationC
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null, true, "/*");
     }
 }
--- src/main/java/ru/illarionov/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/illarionov/springcourse/dao/PersonDAO.java
@@ -31,4 +31,13 @@ public class PersonDAO {
         person.setId(++PEOPLE_COUNT);
         people.add(person);
     }
+
+    public void update(int id, Person updatePerson) {
+        Person personToBoUpdated = show(id);
+        personToBoUpdated.setName(updatePerson.getName());
+    }
+
+    public void delete(int id) {
+        people.removeIf(person -> person.getId() == id);
+    }
 }
