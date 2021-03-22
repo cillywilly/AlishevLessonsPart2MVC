8dccba987bdd7f319693c6a7d11cabe9eaa044e0
AIllarionov <mokolot@mail.ru>
2021-03-22 20:58:25 +0300

    MVC lesson 8 не работает валидация

--- src/main/java/ru/illarionov/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/illarionov/springcourse/controllers/PeopleController.java
@@ -3,9 +3,12 @@ package ru.illarionov.springcourse.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.illarionov.springcourse.dao.PersonDAO;
 import ru.illarionov.springcourse.models.Person;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
@@ -31,12 +34,16 @@ public class PeopleController {
     }
 
     @GetMapping("/new")
-    public String newPerson(@ModelAttribute("person") Person person) {
+    public String newPerson(@ModelAttribute("person")   Person person) {
         return "people/new";
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person) {
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "people/new";
+        }
         personDAO.save(person);
         return "redirect:/people";
     }
@@ -48,8 +55,12 @@ public class PeopleController {
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person,
+    public String update(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult,
                          @PathVariable("id") int id) {
+        if (bindingResult.hasErrors()) {
+            return "people/edit";
+        }
         personDAO.update(id, person);
         return "redirect:/people";
     }
--- src/main/java/ru/illarionov/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/illarionov/springcourse/dao/PersonDAO.java
@@ -13,10 +13,10 @@ public class PersonDAO {
 
     {
         people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, "Tom"));
-        people.add(new Person(++PEOPLE_COUNT, "Alex"));
-        people.add(new Person(++PEOPLE_COUNT, "Sergey"));
-        people.add(new Person(++PEOPLE_COUNT, "Jerry"));
+        people.add(new Person(++PEOPLE_COUNT, "Tom",11,"tom@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Alex",14,"alex@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Sergey",21,"sergey@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Jerry",61,"jerry@mail.ru"));
     }
 
     public List<Person> index() {
@@ -35,6 +35,8 @@ public class PersonDAO {
     public void update(int id, Person updatePerson) {
         Person personToBoUpdated = show(id);
         personToBoUpdated.setName(updatePerson.getName());
+        personToBoUpdated.setAge(updatePerson.getAge());
+        personToBoUpdated.setEmail(updatePerson.getEmail());
     }
 
     public void delete(int id) {
