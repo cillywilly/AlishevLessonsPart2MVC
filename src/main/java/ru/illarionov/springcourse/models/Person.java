0f8e21807306d4e49d87fcff09fd54387d714e7b
aIllarionov <44272886+cillywilly@users.noreply.github.com>
2021-03-23 09:20:45 +0300

    MVC lesson 8 владос помог(версия ибернейт валидатора)

--- src/main/java/ru/illarionov/springcourse/models/Person.java
+++ src/main/java/ru/illarionov/springcourse/models/Person.java
@@ -9,7 +9,7 @@ import javax.validation.constraints.Size;
 public class Person {
     private int id;
     @NotEmpty(message = "Name should not be empty")
-    @Size(min = 2, max = 15, message = "Name should be between 2 and 30 characters")
+    @Size(min = 2, max = 15, message = "Name should be between 2 and 15 characters")
     private String name;
     @Min(value = 0, message = "Age should be greater than 0")
     private int age;
